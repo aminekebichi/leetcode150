b6e8ecfd51e00027f9e2b4d2312c28ba0b6eaf06
Amine Kebichi <amkebichi@gmail.com>
2024-10-15 13:41:38 -0400

    cleaned up codebase specifically around problem122

--- problem122.java
+++ problem122.java
@@ -1,5 +1,4 @@
-package problem122;
-class Solution {
+public class problem122 {
     public static int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 0; i < prices.length - 1; i++) {
@@ -9,12 +8,9 @@ class Solution {
         }
         return profit;
     }
-}
-
-public class problem122 {
     public static void main(String[] args) {
         int[] prices = new int[]{7, 1, 5, 3, 6, 4};
-        if (Solution.maxProfit(prices) == 7) {
+        if (maxProfit(prices) == 7) {
             System.out.println("SUCCESS");
         }
     }
