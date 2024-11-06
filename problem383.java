d0526f3a178aeb0fb24dd3852a038835bfe1ce08
Amine Kebichi <amkebichi@gmail.com>
2024-11-06 18:07:58 -0500

    Problem 383. Ransom Note

---
+++ problem383.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class problem383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : magazine.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()){
+            if(map.getOrDefault(c, 0) == 0) return false;
+            else map.put(c, map.get(c) - 1);
+        }
+
+        return true;
+    }
+}
